588e317ba6e97472b01b2f79babed7f399e445b7
Илья Медведев <0ilya@bk.ru>
2021-10-06 00:58:20 +0300

    add numbers state/voice recogn. and make prettier

--- app.js
+++ app.js
@@ -1,55 +1,69 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import List from './components/list.js';
-import db from './components/firebase.js'
-import { collection, addDoc, getDoc, doc } from 'firebase/firestore';
-import './app.scss';
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
+import List from "./components/list.js"
+import db from "./components/firebase.js"
+import { collection, addDoc, getDoc, doc } from "firebase/firestore"
+import "./app.scss"
 
 function App() {
-    const [state, setState] = useState({
-        0: "editor", //editor or viewer SPLIT to ANOTHER STATE
-        1: "TOMATO",
-        2: "potato",
-        3: "cucumber",
-        4: "moloko"
-    })
-    const [url,setUrl] = useState("")
+  const [state, setState] = useState({
+    0: "editor", //editor or viewer...maybe SPLIT to ANOTHER STATE
+    1: "Молоко",
+    2: "Хлеб",
+    3: "Яйца",
+    4: "Конфеты",
+  })
+  const [count, setCount] = useState({
+    1: 1,
+    2: 2,
+    3: 2,
+    4: 20, //finish here
+  })
+  const [url, setUrl] = useState("")
 
-    useEffect(async()=>{
-        //let params = window.location.pathname.substring(22); git
-        let params = window.location.search.substring(1);
-        if (params!==url) {
-            setUrl(params);
-            let docRef = doc(db, "lists", params);
-            let docSnap = await getDoc(docRef);
-            
-            if (docSnap.exists()) {
-              //console.log("Document data:", data);
-              handleChange({...docSnap.data()});
-            } else {
-              // doc.data() will be undefined in this case
-              console.log("No such document!");
-            }            
-        }
-    })
+  useEffect(async () => {
+    //let params = window.location.pathname.substring(22); git
+    let params = window.location.search.substring(1)
+    if (params !== url) {
+      setUrl(params)
+      let docRef = doc(db, "lists", params)
+      let docSnap = await getDoc(docRef)
 
-    function handleChange(newState) {
-        setState(newState);
-        console.log('main state ',state);
+      if (docSnap.exists()) {
+        //console.log("Document data:", data);
+        handleChange(docSnap.data().state, docSnap.data().count)
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!")
+      }
     }
+  })
 
-    async function sharing() {
-        const docRef = await addDoc(collection(db, "lists"), state);
-        console.log("Document written with ID: ", docRef.id);
-        window.location.search = "?"+docRef.id;
+  function handleChange(newState, newCounter = { 0: -1 }) {
+    if (newCounter[0] !== -1) {
+      setCount(newCounter)
     }
+    setState(newState)
+    console.log("main state ", state)
+    console.log("main count ", count)
+  }
 
-    return (
-        <div className="app">
-            <List state={state} onChange={handleChange} onShare={sharing}/>
-            
-        </div>
-    );
+  async function sharing() {
+    const docRef = await addDoc(collection(db, "lists"), { state, count })
+    console.log("Document written with ID: ", docRef.id)
+    window.location.search = "?" + docRef.id
+  }
+
+  return (
+    <div className="app">
+      <List
+        state={state}
+        count={count}
+        onChange={handleChange}
+        onShare={sharing}
+      />
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
--- components/firebase.js
+++ components/firebase.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp  } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 //import * as admin from "firebase-admin";
 //import * as serviceAccount from "C:/voice-shopping-list-eeerock-firebase-adminsdk-h69yt-28810fb857.json";
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './private.js';
+import { getFirestore } from "firebase/firestore"
+import firebaseConfig from "./private.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,10 +12,10 @@ import firebaseConfig from './private.js';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig());
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-export default db;
+const app = initializeApp(firebaseConfig())
+const db = getFirestore(app)
+const analytics = getAnalytics(app)
+export default db
 /*
 admin.initializeApp({ //add secure
   credential: admin.credential.cert(serviceAccount)
