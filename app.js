0907b9b759d554a51c9b2533e4d1f0317da0b5bc
Terro216 <0ilya@bk.ru>
2021-10-21 21:29:32 +0300

    everything works well

--- app.js
+++ app.js
@@ -9,101 +9,93 @@ import { collection, addDoc, getDoc, doc } from "firebase/firestore"
 import "./app.scss"
 
 function App() {
-  const [state, setState] = useState({
-    0: "editor", //editor or viewer...maybe SPLIT to ANOTHER STATE
-    1: "молоко",
-    2: "хлеб",
-    3: "киви",
-    4: "крендели",
-  })
-  const [count, setCount] = useState({
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4, //finish here
-  })
-  const [modal, setModal] = useState({
-    state: "off",
-    content: "voice",
-  })
-  const [url, setUrl] = useState("")
-  const [addOneState, addOne] = useState({
-    state: false,
-    data: "",
-  })
+	const [state, setState] = useState({
+		0: "editor", //editor or viewer...maybe SPLIT to ANOTHER STATE
+		1: "молоко",
+		2: "хлеб",
+		3: "киви",
+		4: "крендели",
+	})
+	const [count, setCount] = useState({
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4, //finish here
+	})
+	const [modal, setModal] = useState({
+		state: "off",
+		content: "voice",
+		val: "",
+	})
+	const [url, setUrl] = useState("")
+	const [addOneState, addOne] = useState({
+		state: false,
+		data: "",
+	})
 
-  useEffect(async () => {
-    //let params = window.location.pathname.substring(22); git
-    let params = window.location.search.substring(1)
-    if (params !== url) {
-      setUrl(params)
-      let docRef = doc(db, "lists", params)
-      let docSnap = await getDoc(docRef)
+	useEffect(async () => {
+		//let params = window.location.pathname.substring(22); git
+		let params = window.location.search.substring(1)
+		if (params !== url) {
+			setUrl(params)
+			let docRef = doc(db, "lists", params)
+			let docSnap = await getDoc(docRef)
 
-      if (docSnap.exists()) {
-        //console.log("Document data:", data);
-        handleChange(docSnap.data().state, docSnap.data().count)
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!")
-      }
-    }
-  })
+			if (docSnap.exists()) {
+				//console.log("Document data:", data);
+				handleChange(docSnap.data().state, docSnap.data().count)
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!")
+			}
+		}
+	})
 
-  function handleChange(newState, newCounter = {}) {
-    if (newCounter && Object.keys(newCounter).length !== 0) {
-      //if not default
-      setCount(newCounter)
-    }
-    setState(newState)
-    console.log("main state ", state)
-    console.log("main count ", count)
-  }
+	function handleChange(newState, newCounter = {}) {
+		if (newCounter && Object.keys(newCounter).length !== 0) {
+			//if not default
+			setCount(newCounter)
+		}
+		setState(newState)
+		console.log("main state ", state)
+		console.log("main count ", count)
+	}
 
-  async function sharing() {
-    modalToggle({ state: "on", content: "copy" }) //check if working or back it setModal({ state: "on", content: "copy" })
-    //форма копирования открывается в модалке
-    const docRef = await addDoc(collection(db, "lists"), { state, count })
-    console.log("Document written with ID: ", docRef.id)
-    window.location.search = "?" + docRef.id
-    navigator.clipboard.writeText(window.location.href)
-  }
+	async function sharing() {
+		//форма копирования открывается в модалке
+		const docRef = await addDoc(collection(db, "lists"), { state, count })
+		console.log("Document written with ID: ", docRef.id)
+		modalToggle({ state: "on", content: "copy", val: docRef.id }) //check if working or back it setModal({ state: "on", content: "copy" })
+	}
 
-  function modalToggle(e) {
-    console.log(e)
-    setModal({ ...e })
-  }
+	function modalToggle(e) {
+		setModal({ ...e })
+	}
 
-  function addOneToggle(e = 0) {
-    let val = e == 0 ? "" : e //addOneState.data : e
-    console.log("toggle")
-    if (addOneState == false) {
-      addOne({ state: true, data: val })
-    } else {
-      addOne({ state: false, data: val })
-    }
-  }
+	function addOneToggle(e = 0) {
+		let val = e == 0 ? "" : e //addOneState.data : e
+		console.log("tog ", val)
+		if (addOneState.state == false) {
+			addOne({ state: true, data: val })
+		} else {
+			addOne({ state: false, data: val })
+		}
+	}
 
-  return (
-    <div className="app">
-      <Modal modal={modal} closeModal={modalToggle} />
-      <Header state={state} />
-      <List
-        state={state}
-        count={count}
-        showModal={modalToggle}
-        addOneState={addOneState}
-        addOneToggle={addOneToggle}
-      />
-      <Controls
-        state={state}
-        count={count}
-        onChange={handleChange}
-        onShare={sharing}
-        addOneToggle={addOneToggle}
-      />
-    </div>
-  )
+	return (
+		<div className="app">
+			<Modal modal={modal} closeModal={modalToggle} addOneToggle={addOneToggle} />
+			<Header state={state} />
+			<List
+				state={state}
+				count={count}
+				showModal={modalToggle}
+				addOneState={addOneState}
+				addOneToggle={addOneToggle}
+			/>
+			<Controls state={state} onChange={handleChange} onShare={sharing} addOneToggle={addOneToggle} />
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
--- components/modal.js
+++ components/modal.js
@@ -2,117 +2,115 @@ import React, { useEffect } from "react"
 import "./modal.scss"
 
 export default function Modal({ modal, closeModal, addOneToggle }) {
-  useEffect(() => {
-    //console.log(document.getElementById("modal"))
-    if (modal.state === "on") {
-      document.getElementById("modal").style.display = ""
-      document.getElementById("modal-main").innerHTML = ""
-      if (modal.content == "voice") {
-        renderVoice()
-      } else if (modal.content == "copy") {
-        renderCopy()
-      }
-    } else {
-      document.getElementById("modal").style.display = "none"
-    }
-  })
+	useEffect(() => {
+		//console.log(document.getElementById("modal"))
+		if (modal.state === "on") {
+			document.getElementById("modal").style.display = ""
+			document.getElementById("modal-main").innerHTML = ""
+			if (modal.content == "voice") {
+				renderVoice()
+			} else if (modal.content == "copy") {
+				renderCopy(modal.val)
+			}
+		} else {
+			document.getElementById("modal").style.display = "none"
+		}
+	})
 
-  /* voice
+	/* voice
   <div class="voice-micro">
   
   </div>
   <div class="voice-info"></div>
   */
-  function renderVoice() {
-    let main = document.getElementById("modal-main")
-    let micro = document.createElement("div")
-    micro.classList.add("voice-micro")
-    micro.innerHTML = "voice icon"
-    micro.onclick = (e) => {
-      voice()
-    }
-    let info = document.createElement("div")
-    info.classList.add("voice-info")
-    info.innerHTML =
-      "Нажмите на кнопку и перечислите продукты для добавления в список"
+	function renderVoice() {
+		let main = document.getElementById("modal-main")
+		let micro = document.createElement("div")
+		micro.classList.add("voice-micro")
+		micro.innerHTML = "voice icon"
+		micro.onclick = (e) => {
+			voice()
+		}
+		let info = document.createElement("div")
+		info.classList.add("voice-info")
+		info.innerHTML = "Нажмите на кнопку и перечислите продукты для добавления в список"
+		main.append(micro)
+		main.append(info)
+	}
 
-    main.append(micro)
-    main.append(info)
-  }
+	function renderCopy(val) {
+		let newUrl = window.location.origin + "?" + val
+		console.log(newUrl)
 
-  function renderCopy() {
-    let main = document.getElementById("modal-main")
-    let wrapper = document.createElement("div")
-    wrapper.classList.add("copy-wrapper")
-  }
+		navigator.clipboard.writeText(newUrl)
 
-  function voice() {
-    let recognition
-    try {
-      recognition = new webkitSpeechRecognition()
-    } catch (e) {
-      recognition = new SpeechRecognition()
-    }
-    //const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-    //const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
-    recognition.lang = "ru-RU"
-    recognition.interimResults = false
-    recognition.maxAlternatives = 1
+		let main = document.getElementById("modal-main")
+		let wrapper = document.createElement("div")
+		wrapper.classList.add("copy-wrapper")
+		let header = document.createElement("copy-header")
+		header.innerHTML = `<h2>Copyied!</h2>`
+		let copyLine = document.createElement("textarea")
+		copyLine.innerText = newUrl
+		wrapper.append(header)
+		wrapper.append(copyLine)
+		main.append(wrapper)
 
-    recognition.start()
-    console.log("Ready to receive a command.")
+		document.getElementsByClassName("modal-background")[0].onclick = function () {
+			window.location = newUrl
+		}
+	}
 
-    recognition.onaudiostart = function () {
-      //hidden show something
-    }
+	function voice() {
+		let recognition
+		try {
+			recognition = new webkitSpeechRecognition()
+		} catch (e) {
+			recognition = new SpeechRecognition()
+		}
+		//const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+		//const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+		recognition.lang = "ru-RU"
+		recognition.interimResults = false
+		recognition.maxAlternatives = 1
 
-    recognition.onspeechend = function () {
-      recognition.stop()
-      // microphoneWrapper.style.visibility = 'visible'; hidden show again
-      // audioRecordAnimation.style.visibility = 'hidden';
-    }
+		recognition.start()
+		console.log("Ready to receive a command.")
 
-    recognition.onnomatch = function (event) {
-      //alert("Error. Please, try again")
-    }
+		recognition.onaudiostart = function () {
+			//hidden show something
+		}
 
-    recognition.onresult = function (event) {
-      const last = event.results.length - 1
-      let res = event.results[last][0].transcript.split(" ")
-      let list = document.getElementById("list")
+		recognition.onspeechend = function () {
+			recognition.stop()
+			// microphoneWrapper.style.visibility = 'visible'; hidden show again
+			// audioRecordAnimation.style.visibility = 'hidden';
+		}
 
-      console.log("res: ", res)
+		recognition.onnomatch = function (event) {
+			//alert("Error. Please, try again")
+		}
 
-      for (let i = 0; i < res.length; i++) {
-        addOneToggle(res[i])
-      }
+		recognition.onresult = function (event) {
+			const last = event.results.length - 1
+			let res = event.results[last][0].transcript.split(" ")
 
-      closeModal({ state: "off", content: "closed" })
-      /*adding to list (update state)
-      let temp = state
-      let size = Object.keys(temp).length
-      for (let i = 0; i < res.length; i++) {
-        /*if (Number.isInteger(+res[i])) {
-          tempC[sizeC] = +res[i] //3->count but three isnt working
-          sizeC += 1
-        } else {...
-        temp[size] = res[i]
-        size += 1
-      }
-      onChange({ ...temp })
-      */
-      //точность надо? console.log('Confidence: ' + event.results[0][0].confidence);
-    }
-  }
+			console.log("res: ", res)
 
-  return (
-    <div className="modal-wrapper" id="modal">
-      <div
-        className="modal-background"
-        onClick={() => closeModal({ state: "off", content: "closed" })}></div>
-      <div className="modal-main" id="modal-main">
-        hello from modal!
-      </div>
-    </div>
-  )
+			for (let i = 0; i < res.length; i++) {
+				addOneToggle(res[i])
+			}
+
+			closeModal({ state: "off", content: "closed" })
+			//точность надо? console.log('Confidence: ' + event.results[0][0].confidence);
+		}
+	}
+
+	return (
+		<div className="modal-wrapper" id="modal">
+			<div className="modal-background" onClick={() => closeModal({ state: "off", content: "closed" })}></div>
+			<div className="modal-main" id="modal-main">
+				hello from modal!
+			</div>
+		</div>
+	)
 }
